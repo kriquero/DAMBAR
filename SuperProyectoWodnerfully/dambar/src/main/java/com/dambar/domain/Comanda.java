205179d647f5f130ade8eca0f71cb25a56c9e19e
Miquel-Mauri <75072307+Miquel-Mauri@users.noreply.github.com>
2022-01-31 13:47:59 +0100

    Actualizado mesa y comanda
    Se han añadido atributos a las clases mesa y comanda

--- SuperProyectoWodnerfully/dambar/src/main/java/com/dambar/domain/Comanda.java
+++ SuperProyectoWodnerfully/dambar/src/main/java/com/dambar/domain/Comanda.java
@@ -1,5 +1,40 @@
 package com.dambar.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "comandas")
 public class Comanda {
-    
+    @Schema (description = "Identificador", example = "1", required = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Schema(description = "Indica si se ha pagado", example = "1", required = true)
+    @Column
+    private boolean pagado;
+
+    @Schema(description = "Codigo de la mesa", example = "1", required = true)
+    @Column
+    private int codMesa;
+
+    @Schema(description = "Codigo del camarero", example = "1", required = true)
+    @Column
+    private int codCamarero;
+
+    @Schema(description = "Fecha del pedido", example = "", required = true)
+    @Column
+    private LocalDate fechaPedido;
+
+//    @OneToMany(mappedBy = "lineaComanda")
+
+
 }
--- SuperProyectoWodnerfully/dambar/src/main/java/com/dambar/domain/Mesa.java
+++ SuperProyectoWodnerfully/dambar/src/main/java/com/dambar/domain/Mesa.java
@@ -1,10 +1,14 @@
 package com.dambar.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
@@ -12,7 +16,15 @@ import javax.persistence.Entity;
 @Entity(name = "mesas")
 public class Mesa {
 
+    @Schema(description = "identificador", example = "1", required = true)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Schema(description = "Estado de la mesa", example = "Ocupada", required = true)
     estadoMesa estado = estadoMesa.Libre;
+
+
 
 
 }
