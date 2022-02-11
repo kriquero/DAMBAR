77fb6efc77e3ebcb0121b2a5a9a340db3d5178a3
Miquel-Mauri <75072307+Miquel-Mauri@users.noreply.github.com>
2022-02-11 09:41:45 +0100

    Hecha la splash screen
    Aún hay que cambiar cosas pero ya tenemos una splash screen de verdad

--- August twelve (Flutter)/lib/main.dart
+++ August twelve (Flutter)/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:splash_screen/pages/second_screen.dart';
+import 'package:splash_screen/pages/home_page.dart';
 import 'package:splash_screen/pcolors/palette.dart';
 
 void main() {
@@ -19,68 +19,12 @@ class MyApp extends StatelessWidget {
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        primarySwatch: Colors.red,
+        primarySwatch: Palette.kToDark,
         backgroundColor: Colors.grey[800],
       ),
       themeMode: ThemeMode.system,
-      home: MyHomePage(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  void initState() {
-    super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => SecondScreen())));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.only(top: 200, left: 20, right: 20),
-        color: Theme.of(context).colorScheme.background,
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/images/logo.png',
-              color: Colors.white.withOpacity(0.85),
-              colorBlendMode: BlendMode.modulate,
-            ),
-            Text(
-              'August Twelve',
-              style: TextStyle(
-                fontFamily: "CinzelDecorative",
-                color: Colors.white,
-                fontWeight: FontWeight.w700,
-                decoration: TextDecoration.none,
-                fontSize: 30,
-              ),
-            )
-          ],
-        ));
-  }
-}
-
-/*class SecondScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("GeeksForGeeks")),
-      body: Center(
-          child: Text(
-        "Home page",
-        textScaleFactor: 2,
-      )),
-    );
-  }
-}*/
